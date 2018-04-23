ac478979c49a5b9b5b1f3191cd0ffb40ee795774
Nil Ordoñez <nilordonez.7@gmail.com>
2018-04-23 12:07:45 +0200

    Initial commit

---
+++ src/settings.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+
+        //Doctrine settings
+        'doctrine' => [
+            'meta' => [
+                'entity_path' => [
+                    'app/src/Entity'
+                ],
+                'auto_generate_proxies' => true,
+                'proxy_dir' =>  __DIR__.'/../cache/proxies',
+                'cache' => null,
+            ],
+            'connection' => [
+                'driver'   => 'pdo_mysql',
+                'host'     => 'localhost',
+                'dbname'   => 'your-db',
+                'user'     => 'your-user-name',
+                'password' => 'your-password',
+            ]
+        ]
+    ],
+];
