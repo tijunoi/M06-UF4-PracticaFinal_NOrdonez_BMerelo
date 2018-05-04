5f2a836549a7c52291452bea64e035a199ce8ee4
Nil Ordoñez <nilordonez.7@gmail.com>
2018-05-04 03:19:13 +0200

    Getters and setters generated for Notes entity

--- src/Entity/Notes.php
+++ src/Entity/Notes.php
@@ -1,6 +1,7 @@
 <?php
 
 
+namespace Src\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -97,6 +98,200 @@ class Notes
      * @ORM\Column(name="user", type="string", length=20, nullable=true)
      */
     private $user;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param null|string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getPrivate()
+    {
+        return $this->private;
+    }
+
+    /**
+     * @param bool|null $private
+     */
+    public function setPrivate($private)
+    {
+        $this->private = $private;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getTag1()
+    {
+        return $this->tag1;
+    }
+
+    /**
+     * @param null|string $tag1
+     */
+    public function setTag1($tag1)
+    {
+        $this->tag1 = $tag1;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getTag2()
+    {
+        return $this->tag2;
+    }
+
+    /**
+     * @param null|string $tag2
+     */
+    public function setTag2($tag2)
+    {
+        $this->tag2 = $tag2;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getTag3()
+    {
+        return $this->tag3;
+    }
+
+    /**
+     * @param null|string $tag3
+     */
+    public function setTag3($tag3)
+    {
+        $this->tag3 = $tag3;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getTag4()
+    {
+        return $this->tag4;
+    }
+
+    /**
+     * @param null|string $tag4
+     */
+    public function setTag4($tag4)
+    {
+        $this->tag4 = $tag4;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getBook()
+    {
+        return $this->book;
+    }
+
+    /**
+     * @param null|string $book
+     */
+    public function setBook($book)
+    {
+        $this->book = $book;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getCreatedata()
+    {
+        return $this->createdata;
+    }
+
+    /**
+     * @param \DateTime|null $createdata
+     */
+    public function setCreatedata($createdata)
+    {
+        $this->createdata = $createdata;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getLastmodificationdata()
+    {
+        return $this->lastmodificationdata;
+    }
+
+    /**
+     * @param \DateTime|null $lastmodificationdata
+     */
+    public function setLastmodificationdata($lastmodificationdata)
+    {
+        $this->lastmodificationdata = $lastmodificationdata;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param null|string $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+
 
 
 }
