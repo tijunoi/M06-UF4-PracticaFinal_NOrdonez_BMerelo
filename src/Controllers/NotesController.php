5694c9299911f5b9280eaef331ddc23f91f6654b
Nil Ordoñez <nilordonez.7@gmail.com>
2018-05-06 20:11:03 +0200

    - Method getOne implemented.

--- src/Controllers/NotesController.php
+++ src/Controllers/NotesController.php
@@ -50,7 +50,7 @@ class NotesController
         if (!is_null($orderBy)) {
             if (strcmp($orderBy, "titol") == 0) {
                 $orderArray = ["title" => 'ASC'];
-            } else if (strcmp($orderBy,"data") == 0){
+            } else if (strcmp($orderBy, "data") == 0) {
                 $orderArray = ["createData" => 'DESC'];
             }
         }
@@ -63,24 +63,23 @@ class NotesController
         }
 
 
-
         $data = array();
         /** @var Notes $note */
         foreach ($notes as $note) {
 
-            $new = array();
-            $new["id"] = $note->getId();
-            $new["title"] = $note->getTitle();
-            $new["content"] = $note->getContent();
-            $new["private"] = $note->getPrivate();
-            $new["tag1"] = $note->getTag1();
-            $new["tag2"] = $note->getTag2();
-            $new["tag3"] = $note->getTag3();
-            $new["tag4"] = $note->getTag4();
-            $new["book"] = $note->getBook();
-            $new["createData"] = $note->getCreateData();
-            $new["lastModificationData"] = $note->getLastmodificationdata();
-            $data[] = $new;
+            $data = array();
+            $data["id"] = $note->getId();
+            $data["title"] = $note->getTitle();
+            $data["content"] = $note->getContent();
+            $data["private"] = $note->getPrivate();
+            $data["tag1"] = $note->getTag1();
+            $data["tag2"] = $note->getTag2();
+            $data["tag3"] = $note->getTag3();
+            $data["tag4"] = $note->getTag4();
+            $data["book"] = $note->getBook();
+            $data["createData"] = $note->getCreateData();
+            $data["lastModificationData"] = $note->getLastmodificationdata();
+            $data[] = $data;
         }
         $code = 200;
 
@@ -91,8 +90,8 @@ class NotesController
         return $response->withJson($data, $code);
     }
 
-
-    public function getPublicAction(Request $request, Response $response, array $args){
+    public function getPublicAction(Request $request, Response $response, array $args)
+    {
 
         $repo = $this->em->getRepository('Src\Entity\Notes');
 
@@ -103,7 +102,7 @@ class NotesController
         if (!is_null($orderBy)) {
             if (strcmp($orderBy, "titol") == 0) {
                 $orderArray = ["title" => 'ASC'];
-            } else if (strcmp($orderBy,"data") == 0){
+            } else if (strcmp($orderBy, "data") == 0) {
                 $orderArray = ["createData" => 'DESC'];
             }
         }
@@ -116,24 +115,23 @@ class NotesController
         }
 
 
-
         $data = array();
         /** @var Notes $note */
         foreach ($notes as $note) {
 
-            $new = array();
-            $new["id"] = $note->getId();
-            $new["title"] = $note->getTitle();
-            $new["content"] = $note->getContent();
-            $new["private"] = $note->getPrivate();
-            $new["tag1"] = $note->getTag1();
-            $new["tag2"] = $note->getTag2();
-            $new["tag3"] = $note->getTag3();
-            $new["tag4"] = $note->getTag4();
-            $new["book"] = $note->getBook();
-            $new["createData"] = $note->getCreateData();
-            $new["lastModificationData"] = $note->getLastmodificationdata();
-            $data[] = $new;
+            $data = array();
+            $data["id"] = $note->getId();
+            $data["title"] = $note->getTitle();
+            $data["content"] = $note->getContent();
+            $data["private"] = $note->getPrivate();
+            $data["tag1"] = $note->getTag1();
+            $data["tag2"] = $note->getTag2();
+            $data["tag3"] = $note->getTag3();
+            $data["tag4"] = $note->getTag4();
+            $data["book"] = $note->getBook();
+            $data["createData"] = $note->getCreateData();
+            $data["lastModificationData"] = $note->getLastmodificationdata();
+            $data[] = $data;
         }
         $code = 200;
 
@@ -144,7 +142,54 @@ class NotesController
         return $response->withJson($data, $code);
     }
 
-    public function testAction(Request $request, Response $response, array $args){
+    public function getOneAction(Request $request, Response $response, array $args)
+    {
+        $currentUser = $this->auth->getAuthenticatedUser($request);
+
+        $repo = $this->em->getRepository('Src\Entity\Notes');
+
+
+        $id = $request->getQueryParam('id');
+
+        if (is_null($id)) {
+            $data["msg"] = "Missing parameter id";
+            $code = 400;
+        } else {
+            /** @var Notes $note */
+            $note = $repo->find($id);
+
+            if (!is_null($note)) {
+                if ($note->getUser()->getId() == $currentUser->getId() || $note->getPrivate() == false) {
+                    $data = array();
+                    $data["id"] = $note->getId();
+                    $data["title"] = $note->getTitle();
+                    $data["content"] = $note->getContent();
+                    $data["private"] = $note->getPrivate();
+                    $data["tag1"] = $note->getTag1();
+                    $data["tag2"] = $note->getTag2();
+                    $data["tag3"] = $note->getTag3();
+                    $data["tag4"] = $note->getTag4();
+                    $data["book"] = $note->getBook();
+                    $data["user_id"] = $note->getUser()->getId();
+                    $data["user_name"] = $note->getUser()->getNom();
+                    $data["createData"] = $note->getCreateData();
+                    $data["lastModificationData"] = $note->getLastmodificationdata();
+                    $code = 200;
+                } else {
+                    $data["msg"] = "You're not authorized to retrieve this note";
+                    $code = 403;
+                }
+            } else {
+                $data["msg"] = "The note with id $id doesn't exists";
+                $code = 204;
+            }
+        }
+
+        return $response->withJson($data, $code);
+    }
+
+    public function testAction(Request $request, Response $response, array $args)
+    {
 
         $orderBy = $request->getQueryParam('order');
 
@@ -156,7 +201,7 @@ class NotesController
         if (!is_null($orderBy)) {
             if (strcmp($orderBy, "titol") == 0) {
                 $orderArray = ["title" => 'ASC'];
-            } else if (strcmp($orderBy,"data") == 0){
+            } else if (strcmp($orderBy, "data") == 0) {
                 $orderArray = ["createData" => 'DESC'];
             }
         }
